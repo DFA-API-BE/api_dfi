3ac9c5c871931c4098de2b0011337d837765b307
wildanicardi <wildanicardi9@gmail.com>
2022-12-12 18:35:39 +0700

    remove unused import

---
+++ database/models/Employee.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model } from 'sequelize';
+import { dbConnection } from '../config/config';
+import { Users } from './Users';
+
+class Employees extends Model {}
+
+Employees.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.ENUM('Driver', 'Helper'),
+      allowNull: false,
+    },
+    siteCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdBy: {
+      type: DataTypes.STRING,
+    },
+    updatedBy: {
+      type: DataTypes.STRING,
+    },
+    isActive: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: dbConnection,
+    modelName: 'Employees',
+  },
+);
+
+Employees.belongsTo(Users,{
+  foreignKey: 'userId',
+  as: 'user' // this determines the name in `associations`!
+});
+export { Employees };
--- routes/index.ts
+++ routes/index.ts
@@ -3,10 +3,12 @@ import { usersRouter } from './users';
 import { authRouter } from './auth';
 import { vehiclesRoutes } from './vehicles';
 import { checkToken } from './middleware';
+import { employeesRoutes } from './employees';
 const router = express.Router();
 
 router.use('/users', checkToken, usersRouter);
 router.use('/vehicles', checkToken, vehiclesRoutes);
+router.use('/employees', checkToken, employeesRoutes);
 router.use('/auth', authRouter);
 /* GET home page. */
 router.get("/", function (req, res) {
